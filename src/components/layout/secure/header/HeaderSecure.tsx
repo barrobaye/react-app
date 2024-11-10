a0fd6946a6b8dcd91c7adf14c9f052d3b72a3c4f
barrobaye <bayemamourbarro01@gmail.com>
2024-11-10 02:34:59 +0000

    jj -1

--- src/components/layout/secure/header/HeaderSecure.tsx
+++ src/components/layout/secure/header/HeaderSecure.tsx
@@ -5,31 +5,25 @@ export default function HeaderSecure(){
     return(
        
 
-<header className=" flex w-full flex-row-reverse bg-gray-50 dark:bg-gray-900  ">
-<div className="flex justify-between items-center w-full  mx-auto lg:px-8 w-full">
+<header className=" flex  flex-row-reverse bg-gray-50 dark:bg-gray-900  ">
+<div className="flex justify-between items-center w-full  lg:px-8">
     {/* <!-- Start coding here --> */}
-    <div className=" ">
-      <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+    <div className="blocleft">
+      <div className=" flex flex-col items-centerc p-4  md:flex-row md:space-y-0 md:space-x-4">
         <div className="w-full">
           <form className="flex items-center shadow-md dark:bg-gray-800 sm:rounded-lg">
             <label htmlFor="simple-search" className="sr-only">Search</label>
-            <div className="relative w-full">
+            <div className="w-full">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
              
               </div>
-              <input type="text" id="simple-search" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" /> 
+              <input type="text" id="simple-search" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" /> 
             </div>
           </form>
         </div>
-        <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
-        
-          <div className="flex items-center w-full space-x-3 md:w-auto ">
-            <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
-            
-              Actions
-            </button>
-           
-            <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown" className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button"> 
+        <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">      
+          <div className="flex items-center w-full space-x-3 md:w-auto "> 
+            <button  className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button"> 
               Filter
             </button>  
           </div> 
@@ -40,12 +34,14 @@ export default function HeaderSecure(){
     
     
     </div> 
-     <div className="flex items-center">
-                          <img className="rounded-full w-12 h-12" src="../../src/assets/barroprofil.jpeg"/>
+     <div className="flex  blocright  justify-center items-center">
+                         <div className="flex items-center">
+                         <img className="rounded-full w-12 h-12 mr-2" src="../../src/assets/barroprofil.jpeg"/>
                           <div >
                             <strong>Andrew Alfred</strong>
                           </div>
-                          <button type="button" className="text-white h-10 bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Déconnexion</button>
+                         </div>
+                          <button type="button" className="ml-4 text-black h-10 bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Déconnexion</button>
            </div>
   </div>
 
--- src/components/layout/secure/sidebar/SidebarSecure.tsx
+++ src/components/layout/secure/sidebar/SidebarSecure.tsx
@@ -9,39 +9,49 @@ export default function SidebarSecure() {
           <div className="pb-0 h-32 mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
             <img className="mx-auto h-12 w-auto" src="../../src/assets/Leboutiquier.png" />
           </div>
-          <ul className="space-y-2 font-medium">
+          <ul className="space-y-2  mb-2 font-medium">
             <li>
               <NavLink to="/boutique/dashboard" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
                 <i className="fa-solid fa-sliders"></i>          
-                <span className="ms-3">Dashboard</span>
+                <strong className="ms-3">Dashboard</strong>
               </NavLink>
             </li>
             <li>
               <NavLink to="/boutique/article" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
                 <i className="fa-solid fa-tag"></i>  
-                <span className="flex-1 ms-3 whitespace-nowrap">Articles</span>
+                <strong className="flex-1 ms-3 whitespace-nowrap">Articles</strong>
                 <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
               </NavLink>
             </li>
             <li>
               <NavLink to="/boutique/client" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
                 <i className="fa-solid fa-users"></i>
-                <span className="flex-1 ms-3 whitespace-nowrap">Clients</span>
+                <strong className="flex-1 ms-3 whitespace-nowrap">Clients</strong>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/boutique/dette" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+                <i className="fa-solid fa-pen-to-square"></i>
+                <strong className="flex-1 ms-3 whitespace-nowrap">Dettes</strong>
                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
               </NavLink>
             </li>
             <li>
               <NavLink to="/boutique/dette" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
                 <i className="fa-solid fa-hand-holding-dollar"></i>
-                <span className="flex-1 ms-3 whitespace-nowrap">Dettes</span>
+                <strong className="flex-1 ms-3 whitespace-nowrap">Demande</strong>
+                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
               </NavLink>
             </li>
             <li>
-              <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <span className="flex-1 ms-3 whitespace-nowrap">Products</span>
-              </a>
+              <NavLink to="/boutique/dette" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+             <i className="fa-solid fa-user"></i>
+                <strong className="flex-1 ms-3 whitespace-nowrap">Utilisateurs</strong>
+              </NavLink>
             </li>
-            <li>
+          </ul>
+          <ul>
+          <li>
               <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
               </a>
@@ -52,7 +62,7 @@ export default function SidebarSecure() {
               </a>
             </li>
           </ul>
-          <div id="dropdown-cta" className="p-4 mt-6 rounded-lg bg-blue-50 dark:bg-blue-900" role="alert">
+          <div id="dropdown-cta" className="p-4 mt-20 rounded-lg bg-blue-50 dark:bg-blue-900" role="alert">
             <div className="flex items-center mb-3">
               <span className="bg-orange-100 text-orange-800 text-sm font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-900">Beta</span>
               <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-blue-50 inline-flex justify-center items-center w-6 h-6 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800" data-dismiss-target="#dropdown-cta" aria-label="Close">
